aba3a06d41b8d76f2908520e3bd3ce55fff225a7
brayohmurithidev <murithibrianm@gmail.com>
2022-05-04 11:35:50 +0300

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {Route, Routes} from 'react-router-dom';
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import NotFound  from './Pages/NotFound';
+import Admin from "./Pages/Admin/Admin";
+
 
 function App() {
   return (
-    <div className="App">
-      BUS BOOKING SYSTEM
-    </div>
-  );
+  <div>
+    <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route exact path='/admin' element={<Admin />} />
+    <Route path="*" element={<NotFound /> } />
+    </Routes>
+    </div>);
 }
 
 export default App;
---
+++ frontend/src/Pages/NotFound.js
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const NotFound = () => {
+  return (
+    <Typography
+    variant='h3'
+    component='h2'
+    >
+      404! <span>Page Not found</span>
+    </Typography>
+  )
+}
+
+export default NotFound
